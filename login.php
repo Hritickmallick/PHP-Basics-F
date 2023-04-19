55df2e213eba4ae0447b8467cb8fd5ae24b888b0
hritick <hritick.mallick@innoraft.com>
2023-04-19 12:27:45 +0530

    FT-2023-107 ASSIGNMENT7
    created a login page and using sessions redirected it to a new dashboard page
    where user can access all of the assignments through links and after clicking
    on logout it will remove all the session variables and destroy it and the
    user cannot access it without logging in.

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+
+
+<h1>LOGIN PAGE</h1>
+
+<form action="login_bckend.php" method="post">
+<label for="username">ENTER YOUR USERNAME</label>
+<input type="text" name="username" id="username">
+<label for="password">ENTER YOUR PASSWORD</label>
+<input type="password" name="password" id="password">
+
+<input type="submit" name="submit" id="submit">
+
+</form>    
+
+
+</body>
+</html>
